9886a3e68c55e3c4c9cbd2febdc5de3f4fab6192
tamranisamirkhan <tamaranisamirkhan3@gmail.com>
2025-11-21 18:55:25 +0530

    Updated email service to SendGrid and modified application properties
    Signed-off-by: tamranisamirkhan <tamaranisamirkhan3@gmail.com>
    
    Signed-off-by: tamranisamirkhan <tamaranisamirkhan3@gmail.com>

--- src/main/java/com/sam/BankingWebApplication1/Services/ServicesImpl/EmailServiceImpl.java
+++ src/main/java/com/sam/BankingWebApplication1/Services/ServicesImpl/EmailServiceImpl.java
@@ -1,24 +1,66 @@
+//package com.sam.BankingWebApplication1.Services.ServicesImpl;
+//
+//
+//import com.sam.BankingWebApplication1.Services.EmailService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class EmailServiceImpl implements EmailService {
+//
+//    @Autowired
+//    private JavaMailSender mailSender;
+//
+//    @Override
+//    public void sendMail(String to, String subject, String message) {
+//        SimpleMailMessage email = new SimpleMailMessage();
+//        email.setTo(to);
+//        email.setSubject(subject);
+//        email.setText(message);
+//        mailSender.send(email);
+//    }
+//}
 package com.sam.BankingWebApplication1.Services.ServicesImpl;
 
-
 import com.sam.BankingWebApplication1.Services.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${sendgrid.api.key}")
+    private String sendGridApiKey;
 
     @Override
     public void sendMail(String to, String subject, String message) {
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo(to);
-        email.setSubject(subject);
-        email.setText(message);
-        mailSender.send(email);
+        Email from = new Email("smartbankofficial@gmail.com");   // must match SendGrid verified sender
+        Email recipient = new Email(to);
+
+        Content content = new Content("text/plain", message);
+        Mail mail = new Mail(from, subject, recipient, content);
+
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+
+            System.out.println("Email Status Code: " + response.getStatusCode());
+            System.out.println("Email Response Body: " + response.getBody());
+        } catch (IOException ex) {
+            System.out.println("SendGrid Error: " + ex.getMessage());
+        }
     }
 }
