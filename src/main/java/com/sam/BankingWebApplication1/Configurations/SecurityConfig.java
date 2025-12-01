83fa0608b7d9c57fc6f4068f690b97cf7d3a56f3
tamranisamirkhan <tamaranisamirkhan3@gmail.com>
2025-12-01 09:19:38 +0530

    change
    Signed-off-by: tamranisamirkhan <tamaranisamirkhan3@gmail.com>

--- src/main/java/com/sam/BankingWebApplication1/Configurations/SecurityConfig.java
+++ src/main/java/com/sam/BankingWebApplication1/Configurations/SecurityConfig.java
@@ -57,21 +57,24 @@ public class SecurityConfig {
                 .build();
     }
 
-    // ✅ Define allowed origins (your frontend)
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500")
-                        .allowedOrigins("smartbankofficial.netlify.app")
+                        .allowedOrigins(
+                                "http://localhost:5500",
+                                "https://smartbankofficial.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
                         .allowCredentials(true);
             }
         };
     }
+
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
