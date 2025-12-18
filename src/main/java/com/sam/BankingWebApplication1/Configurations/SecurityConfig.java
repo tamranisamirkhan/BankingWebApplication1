a7a165d8aae3769d3962793989e7dad4144f51bd
tamranisamirkhan <tamaranisamirkhan3@gmail.com>
2025-12-18 09:26:42 +0530

    change
    Signed-off-by: tamranisamirkhan <tamaranisamirkhan3@gmail.com>

--- src/main/java/com/sam/BankingWebApplication1/Configurations/SecurityConfig.java
+++ src/main/java/com/sam/BankingWebApplication1/Configurations/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,6 +23,12 @@ public class SecurityConfig {
 
     @Autowired
     private JwtFilter jwtFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
