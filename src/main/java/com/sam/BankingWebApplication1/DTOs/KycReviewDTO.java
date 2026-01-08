c649680aaf1f47396c587a280e9e5c8d8480e49c
tamranisamirkhan <tamaranisamirkhan3@gmail.com>
2026-01-08 10:34:47 +0530

    change
    Signed-off-by: tamranisamirkhan <tamaranisamirkhan3@gmail.com>

--- src/main/java/com/sam/BankingWebApplication1/DTOs/KycReviewDTO.java
+++ src/main/java/com/sam/BankingWebApplication1/DTOs/KycReviewDTO.java
@@ -2,9 +2,16 @@ package com.sam.BankingWebApplication1.DTOs;
 
 import com.sam.BankingWebApplication1.Enums.CustomerStatus;
 import com.sam.BankingWebApplication1.Enums.KycStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class KycReviewDTO {
 
     private Long customerId;
