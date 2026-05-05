6b4ca88f79d2d3febf5fb3f264402594fbd818e7
tamranisamirkhan <tamaranisamirkhan3@gmail.com>
2026-05-05 18:27:54 +0530

    Commit

--- src/main/java/com/sam/BankingWebApplication1/Controllers/TransferController.java
+++ src/main/java/com/sam/BankingWebApplication1/Controllers/TransferController.java
@@ -4,10 +4,16 @@ import com.sam.BankingWebApplication1.DTOs.TransferDTOs.TransferRequest;
 import com.sam.BankingWebApplication1.Services.TransferService;
 import com.sam.BankingWebApplication1.Utils.ResponseModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/smartBank")
+@PreAuthorize("hasRole('CUSTOMER')")
 public class TransferController {
     @Autowired
     private TransferService transferService;
